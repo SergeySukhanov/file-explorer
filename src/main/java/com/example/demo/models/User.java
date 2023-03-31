4bb8acc39f12175812cce454d02e9027f2e37950
Paul Boettger <pauloskar.boettger@gmail.com>
2023-03-31 11:31:32 +0200

    extend User with UserDetails and override in UserDetails specified methods.

--- src/main/java/com/example/demo/models/User.java
+++ src/main/java/com/example/demo/models/User.java
@@ -3,9 +3,11 @@ package com.example.demo.models;
 import com.example.demo.enums.Role;
 import jakarta.persistence.*;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
 
 
 @Entity
@@ -18,40 +20,46 @@ public class User implements UserDetails {
     private String lastName;
     private String password;
 
+    @Enumerated(EnumType.STRING)
     private Role role;
 
+
+    //override methods specified in UserDetails
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return List.of(new SimpleGrantedAuthority(role.name()));
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return email;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
+
+    //getter-setter
+
 }

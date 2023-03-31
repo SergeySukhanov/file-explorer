0ec661628333e9291cfa283980ddc9c9a200f782
Paul Boettger <pauloskar.boettger@gmail.com>
2023-03-31 11:24:17 +0200

    changed user attributes and created Role Enum for User Roles

--- src/main/java/com/example/demo/models/User.java
+++ src/main/java/com/example/demo/models/User.java
@@ -1,4 +1,57 @@
 package com.example.demo.models;
 
-public class User {
+import com.example.demo.enums.Role;
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+
+@Entity
+@Table(name="users")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String password;
+
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }

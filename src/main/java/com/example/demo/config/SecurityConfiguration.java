97ea790b094d4bba72cab0159d91af8505e325a0
Paul Boettger <pauloskar.boettger@gmail.com>
2023-04-03 16:21:57 +0200

    Change in SecurityConfigurations Whitelist
    All endpoints starting with "/api/auth/" are put on the whitelist and therefore do not have to authenticate.

--- src/main/java/com/example/demo/config/SecurityConfiguration.java
+++ src/main/java/com/example/demo/config/SecurityConfiguration.java
@@ -28,7 +28,7 @@ public class SecurityConfiguration {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("")
+                .requestMatchers("/api/auth/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()

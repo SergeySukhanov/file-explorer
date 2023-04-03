c79067de61716e1e95e2beb712ebf1a77091ccda
Paul Boettger <pauloskar.boettger@gmail.com>
2023-04-03 14:19:51 +0200

    Added register and authenticate endpoints to the AuthenticationController.java.
    Only the header of the two POST Methods has been added so far. No actual content.

--- src/main/java/com/example/demo/Controller/AuthenticationController.java
+++ src/main/java/com/example/demo/Controller/AuthenticationController.java
@@ -1,5 +1,24 @@
 package com.example.demo.Controller;
 
+import com.example.demo.requestResponseModels.AuthenticationRequest;
+import com.example.demo.requestResponseModels.AuthenticationResponse;
+import com.example.demo.requestResponseModels.RegisterRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/auth")
 public class AuthenticationController {
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register (@RequestBody RegisterRequest request){
+        return null;
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate (@RequestBody AuthenticationRequest request){
+        return null;
+    }
 }

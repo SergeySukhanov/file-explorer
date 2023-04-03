46558b5bceb4e8be6f53d3a8757f634c0bb8db01
Paul Boettger <pauloskar.boettger@gmail.com>
2023-04-03 15:13:36 +0200

    Added the AuthenticationService.java.
    This class has two methods called "register" and "authenticate".
    
    "register" handles new Register Requests. It saves the new user in the database and returns a jwt.
    
    "authenticate" handles already registered Users. It checks if the credentials are valid and returns a jwt.

---
+++ src/main/java/com/example/demo/JwtAuthentication/AuthenticationService.java
@@ -0,0 +1,53 @@
+package com.example.demo.JwtAuthentication;
+
+import com.example.demo.enums.Role;
+import com.example.demo.models.User;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.requestResponseModels.AuthenticationRequest;
+import com.example.demo.requestResponseModels.AuthenticationResponse;
+import com.example.demo.requestResponseModels.RegisterRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        User user = User.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                request.getEmail(),
+                request.getPassword()
+        ));
+        //TODO handle different possible exceptions
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}

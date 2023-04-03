4fc847e01f391aea6932c91a22926dac7ddde9c0
Paul Boettger <pauloskar.boettger@gmail.com>
2023-04-03 12:20:34 +0200

    Added useful methods to the JwtService Class.
    The Secret_key is currently hardcoded into the class. This could be modified in the future to increase security.

--- src/main/java/com/example/demo/JwtAuthentication/JwtService.java
+++ src/main/java/com/example/demo/JwtAuthentication/JwtService.java
@@ -1,12 +1,40 @@
 package com.example.demo.JwtAuthentication;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
 
+    private static final String SECRET_KEY = "78214125442A472D4B6150645267556B58703273357638792F423F4528482B4D";
+
     public String extractUserEmail(String token) {
 
-        return null;
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSigningKey() {
+        byte [] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }

8c922aba58017a14651a3b2854aa63e5e0bee262
Paul Boettger <pauloskar.boettger@gmail.com>
2023-04-11 18:27:42 +0200

    FolderRepository has three new custom queries:
    -find Folders by Name, Workspace and ParentFolder
    -find Folders by Id and WorkspaceId
    -find Folders by Name

--- src/main/java/com/example/demo/repository/FolderRepository.java
+++ src/main/java/com/example/demo/repository/FolderRepository.java
@@ -1,9 +1,21 @@
 package com.example.demo.repository;
 
 import com.example.demo.models.Folder;
+import com.example.demo.models.Workspace;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface FolderRepository extends CrudRepository<Folder, String> {
+public interface FolderRepository extends CrudRepository<Folder, Long> {
+
+    Optional<Folder> findFolderByNameAndWorkspaceIdAndAndParentFolderId(String name, Workspace workspaceId,Folder parentFolderId);
+
+    Optional<Folder> findFolderByIdAndWorkspaceId(Long id, Workspace workspaceId);
+
+
+
+
+    Optional<Folder> findByName(String name);
 }

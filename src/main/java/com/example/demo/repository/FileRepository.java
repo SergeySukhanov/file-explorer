f22afb4d5ce5147263c7d0192bfb7240bfa8da5a
Paul Boettger <pauloskar.boettger@gmail.com>
2023-04-11 18:22:34 +0200

    The FileRepository got added two new queries:
    -findFileByNameAndWorkspaceIdAnd ParentFolderId
    -findByName

--- src/main/java/com/example/demo/repository/FileRepository.java
+++ src/main/java/com/example/demo/repository/FileRepository.java
@@ -1,10 +1,17 @@
 package com.example.demo.repository;
 
 import com.example.demo.models.File;
+import com.example.demo.models.Folder;
+import com.example.demo.models.Workspace;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface FileRepository extends CrudRepository<File, String> {
+public interface FileRepository extends CrudRepository<File, Long> {
+    Optional<File> findFileByNameAndWorkspaceIdAndParentFolderId(String name, Workspace workspaceId, Folder parentFolderId);
+
+    Optional<File> findByName(String name);
 
 }

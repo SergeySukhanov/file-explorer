32a4771bc9ceb8572f5cefc34b5db307bd94e8d6
Paul Boettger <pauloskar.boettger@gmail.com>
2023-04-11 16:25:02 +0200

    Added some queries to the WorkspaceRepository.java.
    These queries are
    -findByUserIdAndName
    -findAllByUserId

--- src/main/java/com/example/demo/repository/WorkspaceRepository.java
+++ src/main/java/com/example/demo/repository/WorkspaceRepository.java
@@ -1,10 +1,14 @@
 package com.example.demo.repository;
 
+import com.example.demo.models.User;
 import com.example.demo.models.Workspace;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
 public interface WorkspaceRepository extends CrudRepository<Workspace, Long> {
-    Optional<Workspace> findByUserId(Long userId);
+    Optional<Workspace> findByUserId(User userId);
+    Optional<Workspace> findByUserIdAndName(User userId, String name);
+
+    Iterable<Workspace> findAllByUserId(User userId);
 }

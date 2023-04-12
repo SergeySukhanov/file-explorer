b5a340263d67c89c78814d1edeba9c12f9b3fe92
Paul Boettger <pauloskar.boettger@gmail.com>
2023-04-12 14:56:54 +0200

    FileRepository.java has two new queries:
    -findAllByParentFolderId
    -findAllByWorkspaceId

--- src/main/java/com/example/demo/repository/FileRepository.java
+++ src/main/java/com/example/demo/repository/FileRepository.java
@@ -3,6 +3,7 @@ package com.example.demo.repository;
 import com.example.demo.models.File;
 import com.example.demo.models.Folder;
 import com.example.demo.models.Workspace;
+import org.hibernate.jdbc.Work;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,5 +14,8 @@ public interface FileRepository extends CrudRepository<File, Long> {
     Optional<File> findFileByNameAndWorkspaceIdAndParentFolderId(String name, Workspace workspaceId, Folder parentFolderId);
 
     Optional<File> findByName(String name);
+
+    Iterable<File> findAllByParentFolderId(Folder parentFolderId);
+    Iterable<File> findAllByWorkspaceId(Workspace workspaceId);
 
 }
